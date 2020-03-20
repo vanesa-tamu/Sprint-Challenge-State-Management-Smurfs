292b8accef715cbf1c5034417f451976aaa35035
vanesa-tamu <vahe5495@tamu.edu>
2020-03-20 10:30:44 -0500

    added smurf context to main app filef

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import { SmurfContext } from '../contexts/SmurfContext.js'
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState([])
+  return (
+    <div className='App'>
+      <SmurfContext.Provider value={{smurfs}}>
+        <h1>first commit</h1>
+      </SmurfContext.Provider>
+    </div>
+  )
 }
 
 export default App;
